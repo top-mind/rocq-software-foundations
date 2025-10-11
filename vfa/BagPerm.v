44bc815f5e13616134b821d65ce0a0f5a4f988c9
Tiancheng Geng <221900006@smail.nju.edu.cn>
2025-10-12 03:18:49 +0800

    BagPerm

--- vfa/BagPerm.v
+++ vfa/BagPerm.v
@@ -39,21 +39,29 @@ Definition bag_eqv (b1 b2: bag) : Prop :=
 
 Lemma bag_eqv_refl : forall b, bag_eqv b b.
 Proof.
-(* FILL IN HERE *) Admitted.
+  intros. unfold bag_eqv. auto.
+Qed.
 
 Lemma bag_eqv_sym: forall b1 b2, bag_eqv b1 b2 -> bag_eqv b2 b1.
 Proof.
-(* FILL IN HERE *) Admitted.
+  unfold bag_eqv. intros. auto.
+Qed.
 
 Lemma bag_eqv_trans: forall b1 b2 b3, bag_eqv b1 b2 -> bag_eqv b2 b3 -> bag_eqv b1 b3.
 Proof.
-(* FILL IN HERE *) Admitted.
+  unfold bag_eqv.
+  intros.
+  rewrite H.
+  auto.
+Qed.
 
 (** The following little lemma is handy in a couple of places. *)
 
 Lemma bag_eqv_cons : forall x b1 b2, bag_eqv b1 b2 -> bag_eqv (x::b1) (x::b2).
 Proof.
-  (* FILL IN HERE *) Admitted.
+  unfold bag_eqv. intros.
+  simpl. rewrite H. auto.
+Qed.
 (** [] *)
 
 (* ################################################################# *)
@@ -75,14 +83,26 @@ Definition is_a_sorting_algorithm' (f: list nat -> list nat) :=
 
 Lemma insert_bag: forall x l, bag_eqv (x::l) (insert x l).
 Proof.
-(* FILL IN HERE *) Admitted.
+  induction l; simpl.
+  - apply bag_eqv_refl.
+  - bdestruct (x <=? a).
+    + apply bag_eqv_refl.
+    + eapply bag_eqv_trans; [|apply bag_eqv_cons; exact IHl].
+      unfold bag_eqv. simpl. lia.
+Qed.
 (** [] *)
 
 (** **** Exercise: 2 stars, standard (sort_bag)
 
     Now prove that sort preserves bag contents. *)
 Theorem sort_bag: forall l, bag_eqv l (sort l).
-(* FILL IN HERE *) Admitted.
+Proof.
+  induction l.
+  - simpl. apply bag_eqv_refl.
+  - simpl. eapply bag_eqv_trans.
+    + apply bag_eqv_cons. exact IHl.
+    + apply insert_bag.
+Qed.
 (** [] *)
 
 (** Now we wrap it all up.  *)
@@ -99,13 +119,13 @@ Qed.
     of [insert_bag, sort_bag].  Which proofs are simpler?
 
       - [ ] easier with permutations,
-      - [ ] easier with multisets
+      - [X] easier with multisets
       - [ ] about the same.
 
    Regardless of "difficulty", which do you prefer / find easier to
    think about?
       - [ ] permutations or
-      - [ ] multisets
+      - [X] multisets
 
    Put an X in one box in each list. *)
 (* Do not modify the following line: *)
@@ -128,7 +148,13 @@ Definition manual_grade_for_permutations_vs_multiset : option (nat*string) := No
 Lemma perm_bag:
   forall al bl : list nat,
    Permutation al bl -> bag_eqv al bl.
-(* FILL IN HERE *) Admitted.
+Proof.
+  intros. induction H.
+  - apply bag_eqv_refl.
+  - apply bag_eqv_cons. auto.
+  - unfold bag_eqv. simpl. lia.
+  - eapply bag_eqv_trans; eassumption.
+Qed.
 (** [] *)
 
 (** The other direction,
@@ -140,7 +166,12 @@ Lemma perm_bag:
 (** **** Exercise: 2 stars, advanced (bag_nil_inv) *)
 Lemma bag_nil_inv : forall b, bag_eqv [] b -> b = [].
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros. destruct b; auto.
+  unfold bag_eqv in H.
+  specialize H with n.
+  simpl in H. rewrite Nat.eqb_refl in H.
+  lia.
+Qed.
 (** [] *)
 
 (** **** Exercise: 3 stars, advanced (bag_cons_inv) *)
@@ -150,21 +181,50 @@ Lemma bag_cons_inv : forall l x n,
       l = l1 ++ x :: l2
       /\ count x (l1 ++ l2) = n.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  induction l; simpl; intros.
+  - lia.
+  - bdestruct (a =? x).
+    + subst. injection H as H.
+      exists [], l. split; auto.
+    + simpl in H. destruct (IHl _ _ H) as [l1 [l2 [? ?]]].
+      exists (a :: l1), l2. split.
+      * subst. auto.
+      * simpl. rewrite <-Nat.eqb_neq in H0. rewrite H0. auto.
+Qed.
 (** [] *)
 
 (** **** Exercise: 2 stars, advanced (count_insert_other) *)
 Lemma count_insert_other : forall l1 l2 x y,
     y <> x -> count y (l1 ++ x :: l2) = count y (l1 ++ l2).
 Proof.
-  (* FILL IN HERE *) Admitted.
+  induction l1; simpl; intros.
+  - assert (x <> y) by lia.
+    rewrite <-Nat.eqb_neq in H0.
+    rewrite H0. auto.
+  - rewrite IHl1; auto.
+Qed.
 (** [] *)
 
 (** **** Exercise: 3 stars, advanced (bag_perm) *)
 Lemma bag_perm:
   forall al bl, bag_eqv al bl -> Permutation al bl.
 Proof.
-(* FILL IN HERE *) Admitted.
+  induction al; intros.
+  - apply bag_nil_inv in H. subst. auto.
+  - unfold bag_eqv in H.
+    pose proof (H a).
+    simpl in H0. rewrite Nat.eqb_refl in H0.
+    apply bag_cons_inv in H0. destruct H0 as [l1 [l2 [? ?]]].
+    subst.
+    apply Permutation_cons_app.
+    apply IHal.
+    intros n.
+    bdestruct (n =? a).
+    + subst. auto.
+    + rewrite <-count_insert_other with (x:=a); auto.
+      rewrite <- H.
+      simpl. bdestruct (a =? n); try lia.
+Qed.
 (** [] *)
 
 (* ################################################################# *)
